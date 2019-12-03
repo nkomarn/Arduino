c030ce4f64de38492a6cb380fea7ea6ad4a9cecb
Mykyta <nkomarn@hotmail.com>
2019-12-02 21:30:31 -0800

    💡 RGB Led thing.. also I think my ATmega is fried

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Ardunio
-⚡ Arduino stuff and things- mostly just messing around.
+# ⚡ Arduino
+Arduino stuff and things- mostly just messing around. I've only ever used Raspberry Pi, so I hope to learn more about the world of Arduino stuff.
+
+I hope to be able to make something actually... *useful?* some day. So yeah, that's that.
---
+++ RGB_Led/RGB_Led.ino
@@ -0,0 +1,24 @@
+int pins [] = {8, 9, 10};
+
+void setup() {
+  for (int i = 0; i < sizeof(pins); i++) {
+    pinMode(pins[i], OUTPUT);
+  }
+}
+
+void loop() { // Cycle through the RGB spectrum
+  for (int r = 0; r < 254; r++) {
+    for (int g = 0; g < 254; g++) {
+      for (int b = 0; b < 254; b++) {
+        color(r, g, b);
+      }
+    }
+  }
+}
+
+// Set the LED to a specific color
+void color(int red, int green, int blue) {
+  analogWrite(pins[0], red);
+  analogWrite(pins[1], green);
+  analogWrite(pins[2], blue);
+}
